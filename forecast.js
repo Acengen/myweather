3b79c8f49bec2f8c30743ffb5e3f1163ebdfebe0
Acengen <kocic999@yahoo.com>
2020-02-20 16:56:56 +0100

    Updates

---
+++ forecast.js
@@ -0,0 +1,28 @@
+const key = 'LQbWJTqeW7xDbRnuL657u64pGqG74itb';
+
+// get weather info
+
+const getWeather = async (id) => {
+
+const base = 'http://dataservice.accuweather.com/currentconditions/v1/';
+const query = `${id}?apikey=${key}`;
+
+const response = await fetch(base + query);
+const data = await response.json();
+console.log(data);
+return data[0];
+
+};
+
+// get city
+const getCity = async (city) => {
+    const base = 'http://dataservice.accuweather.com/locations/v1/cities/search';
+    const query = `?apikey=${key}&q=${city}`;
+
+    const response = await fetch(base + query);
+    const data = await response.json();
+    console.log(data);
+    return data[0];
+    
+};
+
